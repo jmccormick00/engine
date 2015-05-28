27d461b79a5934c5fab481120307926450a40b53
jmccormick00 <jmccormick00@gmail.com>
2015-05-28 14:19:24 -0400

    changed variable names

--- engine/C_Timer.h
+++ engine/C_Timer.h
@@ -10,19 +10,19 @@ private:
     typedef std::chrono::high_resolution_clock Clock;
     static const long COUNTSPERSECOND = 1e9;
 
-    bool d_bPaused;
+    bool paused_;
 
-    Clock::time_point	d_baseTime,		// The base time of the timer.
-                        d_pauseTime,	// The time marker when the timer was paused
-                        d_prevTime,		// The time during the last time step
-                        d_currentTime;
+    Clock::time_point	baseTime_,		// The base time of the timer.
+                        pauseTime_,	// The time marker when the timer was paused
+                        prevTime_,		// The time during the last time step
+                        currentTime_;
 
-    std::chrono::nanoseconds d_pauseTotal;	// The total time the timer was paused
-    std::chrono::nanoseconds d_deltaTime;	// The delta seconds between ticks
+    std::chrono::nanoseconds pauseTotal_;	// The total time the timer was paused
+    std::chrono::nanoseconds deltaTime_;	// The delta seconds between ticks
 
 public:
     // Constructor
-    C_Timer() : d_bPaused(false), d_pauseTotal(std::chrono::nanoseconds::zero()) {}
+    C_Timer() : paused_(false), pauseTotal_(std::chrono::nanoseconds::zero()) {}
     ~C_Timer() {}
 
     // Gets time in seconds since Reset() was called
@@ -31,8 +31,8 @@ public:
         //	
         // -----*----------------*--------------*----------> Time
         //	d_baseTime		d_pauseTime	    d_currentTime
-        if (d_bPaused)
-            return (double)(d_pauseTime - d_baseTime).count()/COUNTSPERSECOND;
+        if (paused_)
+            return (double)(pauseTime_ - baseTime_).count()/COUNTSPERSECOND;
 
         // The (d_currentTime - d_baseTime) includes any paused time
         // So need to subract that out
@@ -40,63 +40,63 @@ public:
         // -----*----------------*-------------------*----------------*----> Time
         //	d_baseTime		d_pauseTime	           start()        d_currentTime
         else {
-            d_currentTime = Clock::now();
-            return (double)(d_currentTime - d_baseTime - d_pauseTotal).count()/COUNTSPERSECOND;
+            currentTime_ = Clock::now();
+            return (double)(currentTime_ - baseTime_ - pauseTotal_).count()/COUNTSPERSECOND;
         }
     }
 
     // Gets the time in seconds since last tick()
     inline double getDeltaTime() {
-        return (double)d_deltaTime.count()/COUNTSPERSECOND;
+        return (double)deltaTime_.count()/COUNTSPERSECOND;
     }
 
     // Called to zero the timer and start the counters
     inline void start() {
-        d_currentTime = Clock::now();
-        d_baseTime = d_currentTime;
-        d_prevTime = d_currentTime;
-        d_pauseTotal = std::chrono::nanoseconds::zero();
-        d_bPaused = false;
+        currentTime_ = Clock::now();
+        baseTime_ = d_currentTime;
+        prevTime_ = d_currentTime;
+        pauseTotal_ = std::chrono::nanoseconds::zero();
+        paused_ = false;
     }
 
     // Called to pause the timer
     inline void pause() {
         // If its already paused, don't do anything
-        if (d_bPaused) return;
+        if (paused_) return;
         // Save the time we stopped at
-        d_pauseTime = Clock::now();
-        d_bPaused = true;
+        pauseTime_ = Clock::now();
+        paused_ = true;
     }
     
     inline void unpause() {
-        if(!d_bPaused) return;
+        if(!paused_) return;
         // We are resuming from a stopped state
         // accumalate the paused time
         //
         //				|<--d_pauseTotal--->|
         // -------------*-------------------*--------------> Time
         //			d_pauseTime			 start()
-        d_currentTime = Clock::now();
-        d_pauseTotal += (d_currentTime - d_pauseTime);
+        currentTime_ = Clock::now();
+        pauseTotal_ += (currentTime_ - pauseTime_);
         
         // Reset the previous time
-        d_prevTime = d_currentTime;
-        d_bPaused = false;
+        prevTime_ = currentTime_;
+        paused_ = false;
     }
 
     // Called before the scene is updated, at the beginning of the sim loop
     inline void tick() {
         // If paused do nothing
-        if (d_bPaused) return;
+        if (paused_) return;
 
         // Calculate delta
         //
         //				|<----d_deltaTime----->|
         // -------------*----------------------*--------------> Time
         //			d_iPrevTime			      Tick()
-        d_currentTime = Clock::now();
-        d_deltaTime = d_currentTime - d_prevTime;
-        d_prevTime = d_currentTime;
+        currentTime_ = Clock::now();
+        deltaTime_ = currentTime_ - prevTime_;
+        prevTime_ = currentTime_;
     }
 };
 
